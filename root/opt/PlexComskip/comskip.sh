ea91e6a4900a3b4efe1166563d2596e9688bc39c
DasPflanze <DasPflanze@users.noreply.github.com>
2017-08-08 11:00:43 +0200

    add comskip.sh

---
+++ root/opt/PlexComskip/comskip.sh
@@ -0,0 +1,40 @@
+#!/bin/csh
+
+set path = ($path /usr/local/bin)
+set lockFile = '/tmp/dvrProcessing.lock'
+set origFile = "$1"
+set tmpFile = "$1.tmp"
+set dvrPostLog = '/tmp/dvrProcessing.log'
+set presetFile = '/opt/PlexComskip/PlexScriptPreset.json'
+set SUFF = 'ts'
+set suff = 'mp4'
+
+#Wait if post processing is already running
+while ( -f $lockFile )
+    echo "'$lockFile' exists, sleeping processing of '$origFile'" | tee $dvrPostLog
+    sleep 10
+end
+
+#Create lock file to prevent other post-processing from running simultaneously
+echo "Creating lock file for processing '$origFile'" | tee -a $dvrPostLog
+touch $lockFile
+
+#Remove commercials
+echo "Removing commercials from '$origFile'" | tee -a $dvrPostLog
+cp "$origFile" "$origFile.backup"
+/comchap/comcut --ffmpeg=/usr/bin/ffmpeg --comskip=/usr/local/bin/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/opt/PlexComskip/comskip.ini "$origFile"                                                                               $
+
+#Encode file to H.264 with mkv container using Handbrake
+echo "Re-encoding '$origFile' to '$tmpFile'" | tee -a $dvrPostLog
+nice -20 HandBrakeCLI --preset-import-file $presetFile -Z "Plex Script" -i "$origFile" -o "$tmpFile"
+
+#Overwrite original mkv file with the transcoded file.
+echo "Renaming '$tmpFile' to '$origFile'" | tee -a $dvrPostLog
+mv -f "$tmpFile" "$origFile"
+mv -f "$origFile" "$origFile:r.mp4"
+
+#Remove lock file
+echo "Done processing '$origFile' removing lock" | tee -a $dvrPostLog
+rm $lockFile
+
+exit 0
